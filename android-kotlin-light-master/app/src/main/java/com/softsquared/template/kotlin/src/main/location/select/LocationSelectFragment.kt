efa957dc0852559a8b879a8fcd57b1d93d02ce6e
RyuSw-cs <rsw1452@naver.com>
2022-04-05 14:20:44 +0900

    fix: #1 즐겨찾기 오류 수정

--- android-kotlin-light-master/app/src/main/java/com/softsquared/template/kotlin/src/main/location/select/LocationSelectFragment.kt
+++ android-kotlin-light-master/app/src/main/java/com/softsquared/template/kotlin/src/main/location/select/LocationSelectFragment.kt
@@ -110,7 +110,7 @@ class LocationSelectFragment(private val addressData: HomeAddressResponse) :
 
                 //만약 선택? -> 체크 표시 on
                 if (addressData.result.nowAddress.userAddressIdx == addressData.result.companyAddress.userAddressIdx) {
-                    ivCheck.visibility = View.VISIBLE
+                    ivCompanyCheck.visibility = View.VISIBLE
                 }
             }
         }
